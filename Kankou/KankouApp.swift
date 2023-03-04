5693e412c53be1f40359f8cd5e8867ab042ff89e
znak99 <xhahekcldhkznak0113@gmail.com>
2023-03-04 22:33:55 +0900

    Initial Commit

---
+++ Kankou/KankouApp.swift
@@ -0,0 +1,17 @@
+//
+//  KankouApp.swift
+//  Kankou
+//
+//  Created by seungwoo on 2023/03/04.
+//
+
+import SwiftUI
+
+@main
+struct KankouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
