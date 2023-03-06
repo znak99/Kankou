c25c225b54c8e77caa4fa967925ad7a26ddce78d
znak99 <xhahekcldhkznak0113@gmail.com>
2023-03-06 11:11:57 +0900

    Add fonts

---
+++ Kankou/Extensions/FontExtension.swift
@@ -0,0 +1,20 @@
+//
+//  FontExtension.swift
+//  Kankou
+//
+//  Created by seungwoo on 2023/03/06.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    static let customFredoka = "FredokaOne-Regular"
+    static let customBlack = "RoundedMplus1c-Black"
+    static let customBold = "RoundedMplus1c-Bold"
+    static let customExtraBold = "RoundedMplus1c-ExtraBold"
+    static let customLight = "RoundedMplus1c-Light"
+    static let customMedium = "RoundedMplus1c-Medium"
+    static let customRegular = "RoundedMplus1c-Regular"
+    static let customThin = "RoundedMplus1c-Thin"
+}
